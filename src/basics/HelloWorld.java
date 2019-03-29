04b24347b6fd1bd7f84a5291977d9fa9d09933d1
gunt <gunt.fl@gmail.com>
2019-03-29 13:04:16 +0100

    Modified Helloworld app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,8 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		System.out.println("I'm adding a code to my project");
 	}
 
 }
